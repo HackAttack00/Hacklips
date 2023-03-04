4147f3ed479a8477cbb0399c349c73d86906c70d
hack_이승철 <sclee@afreecatv.com>
2023-03-04 15:16:26 +0900

    macoos target 13, add menuExtraView

--- Hacklips/HacklipsApp.swift
+++ Hacklips/HacklipsApp.swift
@@ -10,11 +10,35 @@ import SwiftUI
 @main
 struct HacklipsApp: App {
     let persistenceController = PersistenceController.shared
-
+    
+    @State var currentNumber: String = "1"
+    
     var body: some Scene {
+        
+        MenuBarExtra("Hacklips", systemImage: "hammer") {
+            clipMenuBarExtraView()
+        }
+        
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
+
+struct clipMenuBarExtraView: View {
+    var body: some View {
+        Button(action: action1, label: { Text("Action 1") })
+        Button(action: action2, label: { Text("Action 2") })
+        
+        Divider()
+
+        Button(action: action3, label: { Text("Action 3") })
+    }
+    
+    func action1() {}
+    func action2() {}
+    func action3() {}
+
+}
+    
