20383a29d90c9975b74f23d21037fccab46ab8e6
hack_이승철 <sclee@afreecatv.com>
2023-03-07 00:09:46 +0900

    polling으로 클립보드 텍스트 가져옴

---
+++ Hacklips/Data/ClipboardWatcher.swift
@@ -0,0 +1,42 @@
+//
+//  ClipboardWatcher.swift
+//  Hacklips
+//
+//  Created by Seungchul Lee on 2023/03/06.
+//
+
+import Cocoa
+
+class ClipboardWatcher: NSObject {
+    private var lastChangeCount = NSPasteboard.general.changeCount
+    private var timer: Timer?
+    
+    override init() {
+        super.init()
+    }
+    
+    func startWatcher() {
+        if let timer = timer {
+            timer.invalidate()
+            self.timer = nil
+        }
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.checkClipboard()
+        }
+    }
+    
+    private func checkClipboard() {
+        guard NSPasteboard.general.changeCount != lastChangeCount else {
+            return
+        }
+        lastChangeCount = NSPasteboard.general.changeCount
+        if let text = NSPasteboard.general.string(forType: .string) {
+            handleClipboardChange(text: text)
+        }
+    }
+    
+    func handleClipboardChange(text: String) {
+        print("handleClipboardChange = \(text)")
+    }
+}
